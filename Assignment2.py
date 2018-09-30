6b4e26cb23836f75787676bbd71e2c5e3c1fe49f
Naveen Shandilya <40190745+naveenshandilya30@users.noreply.github.com>
2018-09-30 18:21:25 +0545

    Intro to Python

---
+++ Assignment2.py
@@ -0,0 +1,36 @@
+#Que_1
+
+print("Hello")
+
+#Que_2
+
+str1 = "Acad"
+str2 = "View"
+str3 = str1 + str2
+print (str3)
+
+#Que_3
+
+x="Hello"
+y="my name is Naveen"
+z="Iam from Kasauli "
+print (x,y,z)
+
+#Que_4
+
+print('Let\'s get started')
+
+#Que_5
+
+s="Acadview"
+course="python"
+fees=5000
+print ('%s %s %d' % (s,course,fees))
+
+#Que_6
+
+import math
+
+pi=3.14
+r=float(input("Enter radius of circle: "))
+print('Area of circle is: ',pi*r*r)
