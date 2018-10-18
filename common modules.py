5c0d145ab325746d4d75fadf8dd1aed5ccf3b82f
Naveen Shandilya <40190745+naveenshandilya30@users.noreply.github.com>
2018-10-18 13:56:43 +0530

    common modules

---
+++ common modules.py
@@ -0,0 +1,29 @@
+#Q.1
+
+import datetime
+
+today = datetime.date.today()
+print (today)
+
+
+#Q.2
+import webbrowser
+import time
+total_breaks=3
+break_count=0
+while(break_count<total_breaks):
+    print("This program started on"+time.ctime())
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=OD7d1e1mwyQ")
+    break_count +=1
+
+    
+#Q.3
+import os 
+path = r'C:\Users\Naveen\Desktop'
+files = os.listdir(path)
+i = 1
+
+for file in files:
+    os.rename(os.path.join(path, file), os.path.join(path, str(i)+'.jpg'))
+    i = i+1     
